9f7b1c641d427832331896c12238da3c733abf6d
sebasGZA <sebassga34@gmail.com>
2026-06-26 17:42:03 -0500

    feat: inputs to create characters

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,4 @@
-import { NgClass } from '@angular/common';
+// import { NgClass } from '@angular/common';
 import { Component, computed, signal } from '@angular/core';
 
 interface Character {
@@ -13,6 +13,10 @@ interface Character {
   // imports: [NgClass]
 })
 export class DragonballComponent {
+
+  name = signal('')
+  power = signal(0)
+
   characters = signal<Character[]>([
     {
       id: 1,
@@ -29,16 +33,33 @@ export class DragonballComponent {
       name: 'Trunks',
       power: 8000
     },
-        {
+    {
       id: 4,
       name: 'Yamcha',
       power: 500
     },
   ]);
 
+  addCharacter() {
+    if (this.name() && this.power() > 0) {
+      const character: Character = {
+        id: this.characters().length + 1,
+        name: this.name(),
+        power: this.power()
+      }
+      this.characters.update((list) => [...list, character])
+      this.resetFields()
+    }
+  }
+
   powerClasses = computed(() => {
     return {
       'text-danger': true,
     }
   })
+
+  resetFields(){
+    this.name.set('')
+    this.power.set(0)
+  }
 }
