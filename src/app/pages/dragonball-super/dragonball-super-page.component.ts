9c14ec2e79610c3cef8837dc19c17727ecd8ac9d
sebasGZA <sebassga34@gmail.com>
2026-06-26 18:56:05 -0500

    feat: outputs bases

--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,18 +1,14 @@
-// import { NgClass } from '@angular/common';
-import { Component, computed, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CharacterListComponent } from "../../components/dragonball/character-list/character-list.component";
 import { Character } from '../../interfaces/character.interface';
+import { AddCharacterComponent } from "../../components/dragonball/add-character/add-character.component";
 
 @Component({
   selector: 'app-dragonball-super',
   templateUrl: './dragonball-super-page.component.html',
-  imports: [CharacterListComponent],
-  // imports: [NgClass]
+  imports: [CharacterListComponent, AddCharacterComponent],
 })
 export class DragonballSuperComponent {
-
-  name = signal('')
-  power = signal(0)
 
   characters = signal<Character[]>([
     {
@@ -27,26 +23,7 @@ export class DragonballSuperComponent {
     },
   ]);
 
-  addCharacter() {
-    if (this.name() && this.power() > 0) {
-      const character: Character = {
-        id: this.characters().length + 1,
-        name: this.name(),
-        power: this.power()
-      }
-      this.characters.update((list) => [...list, character])
-      this.resetFields()
-    }
-  }
-
-  powerClasses = computed(() => {
-    return {
-      'text-danger': true,
-    }
-  })
-
-  resetFields(){
-    this.name.set('')
-    this.power.set(0)
+  addCharacter(character: Character) {
+    this.characters.update(list => [...list, character])
   }
 }
