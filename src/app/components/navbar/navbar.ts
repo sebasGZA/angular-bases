327ba210f4218f2bf5eb9a04a2524742ba153a5f
sebasGZA <sebassga34@gmail.com>
2026-06-26 16:34:20 -0500

    feat: router link active

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.html',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
 })
 export class Navbar {}
